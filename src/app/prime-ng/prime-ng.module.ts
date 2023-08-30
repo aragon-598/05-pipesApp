c26c110e18872f055b076dcbd8919f245149b6ad
Alejandro Aragon <aragondru155@gmail.com>
2023-08-30 14:37:06 -0600

    adding primeng and menubar

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+@NgModule({
+  declarations: [],
+  imports:[
+    MenuModule
+  ],
+  exports: [
+    MenuModule,
+    MenubarModule
+  ]
+})
+export class PrimeNgModule { }
